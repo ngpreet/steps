2a945d8a1015576ba678f4ce0081b0df12d64020
ngpreet <ng.navpreet@gmail.com>
2023-08-21 11:16:18 +0530

    steps state and event handler

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,7 +7,16 @@ const messages = [
 ];
 
 function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -13,12 +24,18 @@ function App() {
         <div className={step >= 2 ? "active" : ""}>2</div>
         <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
-      <p className="message">{messages[step - 1]}</p>
+      <p className="message">Step {step}: {messages[step - 1]}</p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
