52263488461e3f558874846ce7cdf9fbca527812
ngpreet <ng.navpreet@gmail.com>
2023-09-11 10:24:07 +0530

    reusable button and children prop

--- src/App.js
+++ src/App.js
@@ -24,22 +24,29 @@ function App() {
         <div className={step >= 2 ? "active" : ""}>2</div>
         <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
-      <p className="message">Step {step}: {messages[step - 1]}</p>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
       <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
+        <Button bgColor="#7950f2" color="#fff" onClick={handlePrevious}>
+          <span>👈</span>Previous
+        </Button>
+        <Button bgColor="#7950f2" color="#fff" onClick={handleNext}>
+          Next<span>👉</span>
+        </Button>
       </div>
     </div>
+  );
+}
+
+function Button({ bgColor, color, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: color }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
 
